13f5263b753f71ae48e83d222347c30d0d1e9943
tiagodsilva <tdsh97@gmail.com>
2022-03-31 13:59:24 -0300

    Insert method to initialize regions

--- objects/regions.cpp
+++ objects/regions.cpp
@@ -19,6 +19,11 @@ class Region {
 		Map * map; // the map in which the game is happening 
 		// a mutex 
 	public: 
+		/**  
+		* Constructor method for `Region`; it is designed to be a mutex keeper.  
+		* @param int xRegion, yRegion, xOffsetRegion, yOffsetRegion the offsets that characterize the region 
+		* @param Map * map the map to which we tied the tiles 
+		*/  
 		Region(int xRegion, int yRegion, 
 				int xOffsetRegion, int yOffsetRegion, Map * mapRegion) 
 			: x(xRegion), y(yRegion), xOffset(xOffsetRegion), yOffset(yOffsetRegion), 
@@ -26,10 +31,16 @@ class Region {
 				// Identify the tiles in the region charecterized by the provided coordinates 
 				for (int col = x; col < x + xOffset; col++) { 
 					for (int row = y; row < y + Offset; row++) { 
+						// Check whether the tile is consistent with the 
+						// map boundaries 
 						if (Tile tile = map->getTile(col, row)) 
 						       tiles.push(tile); 	
 					 } 
 				} 
 		} 
+
+		// It should contain functions to control simultaneous access to shared data  
+		
+		// mutex functions 
 } 
 
