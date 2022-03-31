69973886e8a8713b0c4bb2e0588046aa048dd2db
tiagodsilva <tdsh97@gmail.com>
2022-03-31 01:32:08 -0300

    I'm sleepy; but I am going to implement a adequate vector class tomorrow

--- utils/geometry.cpp
+++ utils/geometry.cpp
@@ -18,4 +18,14 @@ float dotProduct(Vec v, Vec w) {
 	return v.x * w.x + v.y * w.y; 
 } 
 
+float distanceToSegment(Vec v, Vec w, Vec p) { 
+	// Compute the distance between v and w -- to assert that the 
+	// segment is actually a segment 
+	const float squaredDistance = lengthSquared(v, w); 
+	if (float < 1e-19) return distance(v, w); 
+	// https://stackoverflow.com/questions/849211
+	const float alpha = max(0, min(1, dot(p - v, w - v)/squaredDistance)); 
+	const Vec projection = v + alpha * (w - v); 
+	return distance(p. projection); 
+} 
 
