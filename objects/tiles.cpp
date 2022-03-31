5034b3aa1c44213c8870c58fb82462f32f13e67e
tiagodsilva <tdsh97@gmail.com>
2022-03-30 22:08:29 -0300

    Insert method to kill colony

--- objects/tiles.cpp
+++ objects/tiles.cpp
@@ -1,5 +1,6 @@
 #include <iostream> 
 #include <vector> 
+#include <queue> 
 #include "ants.cpp" 
 
 class Tile { 
@@ -10,7 +11,7 @@ class Tile {
 	
 		// An array containg the arrays in the current tile 
 		std::map<std::string, 
-			std::vector<Ant>> ants; 
+			std::queue<Ant>> ants; 
 	public: 
 		/**  
 		* Insert an ant in the array `ants`; we should be carefull to 
@@ -23,11 +24,20 @@ class Tile {
 
 			// Check whether there is already an key correspoding to the 
 			// colony with name `colony` 
-			if (ants.colony(colony) == colony.end()) { 
-				ants[colony] = new std::vector<Ant>;  
+			if (ants.find(colony) == ants.end()) { 
+				ants[colony] = new std::queue<Ant>;  
 			} else { 
 				ants[colony].push(ant); 
 			} 
+		} 
+
+		void killAnt(Ant * ant) { 
+			// The ants from the same colony are indistinguishable 
+			std::string colony = ant->colony->name; 
+
+			// And, in particular, we should extract any of them from the 
+			// corresponding queue 
+			ants[colony].pop(); 
 		} 
 
 } 
