1838a1002a92a22d4cbdfd567e3529087528849e
tiagodsilva <tdsh97@gmail.com>
2022-03-31 09:59:56 -0300

    Update documentation

--- utils/geometry.cpp
+++ utils/geometry.cpp
@@ -40,8 +40,11 @@ float distanceToSegment(Vec v, Vec w, Vec p) {
 	const float squaredDistance = lengthSquared(v, w); 
 	if (float < 1e-19) return distance(v, w); 
 	// https://stackoverflow.com/questions/849211
+	// We are clipping the parameter `alpha` to the unitary interval in the real line, 
+	// since we aim to identify a point in the segment; precisely, we are tackling the 
+	// scenario in which the point is not in the segment. 
 	const float alpha = max(0, min(1, dot(p - v, w - v)/squaredDistance)); 
 	const Vec projection = v + alpha * (w - v); 
-	return distance(p. projection); 
+	return distance(p, projection); 
 } 
 
