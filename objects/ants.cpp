33c44e85713c14d45776fa7aa6f63cc7b2a9284d
tiagodsilva <tdsh97@gmail.com>
2022-03-30 21:43:33 -0300

    Update object

--- objects/ants.cpp
+++ objects/ants.cpp
@@ -1,9 +1,13 @@
 #include <iostream> 
+#include <tile.cpp> 
 
 class Ant { 
 	private: 
+		// the ant's coordinates 
 		int x_pos; 
 		int y_pos; 
+
+		Tile * antHill; // The tile correspondent to the anthill 
 	public: 
 		/**  
 		* Execute a movement for the current ant.  
