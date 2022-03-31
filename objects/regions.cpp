970c76d17b48c7aef0ed5e77bde090f03cb41b39
tiagodsilva <tdsh97@gmail.com>
2022-03-31 13:56:30 -0300

    Insert a vector of tiles in the region

--- objects/regions.cpp
+++ objects/regions.cpp
@@ -15,9 +15,21 @@ class Region {
 		// The horizontal and vertical offsets 
 		int xOffset; 
 		int yOffset; 
-
+		
+		Map * map; // the map in which the game is happening 
 		// a mutex 
 	public: 
-
+		Region(int xRegion, int yRegion, 
+				int xOffsetRegion, int yOffsetRegion, Map * mapRegion) 
+			: x(xRegion), y(yRegion), xOffset(xOffsetRegion), yOffset(yOffsetRegion), 
+			mapRegion(map) { 
+				// Identify the tiles in the region charecterized by the provided coordinates 
+				for (int col = x; col < x + xOffset; col++) { 
+					for (int row = y; row < y + Offset; row++) { 
+						if (Tile tile = map->getTile(col, row)) 
+						       tiles.push(tile); 	
+					 } 
+				} 
+		} 
 } 
 
