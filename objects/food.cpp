637cfd87eb092bd671a4819cb9f0b3acb566a901
tiagodsilva <tdsh97@gmail.com>
2022-04-02 22:10:06 -0300

    Update Food class

--- objects/food.cpp
+++ objects/food.cpp
@@ -1,42 +1,33 @@
 #include <iostream> 
 #include <vector> 
 
-class Food { 
-	private: 
-		Tile * tile; // the tile in which the food was placed in the game 
-		int volume; // the amount of volume in this food; it is measured in arbitrary units 
-
-		int initialVolume; // The initial volume of food in this tile; 
-		// it is appropriate for the restoration 
-	public: 
-		/**  
-		* Constructor method for `Food`.  
-		* @param Tile * foodTile the tile in which it was placed in the game 
-		* @param int initVolume the initial volume of the food in this tile 
-		*/  
-		Food(Tile * foodTile, int initVolume) 
-			: tile(foodTile), initialVolume(initVolume), volume(initVolume) 
-			{} 
+/**  
+* Constructor method for `Food`.  
+* @param Tile * foodTile the tile in which it was placed in the game 
+* @param int initVolume the initial volume of the food in this tile 
+*/  
+Food(Tile * foodTile, int initVolume) 
+	: tile(foodTile), initialVolume(initVolume), volume(initVolume) 
+	{} 
 		
-		/**  
-		* Consume the food; it should be designed as in the Philosophers' dilemma (eat or don't eat).  
-		* @return true if there was food to be consumed; in the other scenario, `false` is the variable. 
-		*/  
-		bool consume() { 
-			if (volume >= 1) {  
-				volume--; 
-				return true; 
-			} else { 
-				return false; 
-			} 
-		} 
+/**  
+* Consume the food; it should be designed as in the Philosophers' dilemma 
+* (eat or don't eat).  
+* @return true if there was food to be consumed; 
+* in the other scenario, `false` is the variable. 
+*/  
+bool consume() { 
+	if (volume >= 1) {  
+		volume--; 
+		return true; 
+	} else { 
+		return false; 
+	} 
+} 
 
-		/**  
-		* Restore the initial volume, as asserted in the parameter `initialVolume`.  
-		*/  
-		void restore() { 
-			volume = initialVolume; 
-		} 
-
-			
+/**  
+* Restore the initial volume, as asserted in the parameter `initialVolume`.  
+*/  
+void restore() { 
+	volume = initialVolume; 
 } 
