5a9da3e797b11c556cd37516ca0a2d91e256fd47
tiagodsilva <tdsh97@gmail.com>
2022-04-02 21:16:02 -0300

    Insert header file for tiles

---
+++ objects/headers/tiles.hpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+class Tile { 
+	private: 
+		// Tile's coordinates 
+		int x; 
+		int y; 
+
+		// Array with the ants in this tile for each colony 
+		std::map<std::string, std::queue<Ant*>> ants; 
+
+		// Ramifications of this tile 
+		bool isAnthill; 
+		bool isFood; 
+
+	public: 
+		int pheromone; // the intensity of pheromone in these coordinates 
+
+		Tile(int x_tile, int y_tile, bool containsAnthill, bool containsFood); 
+		void insertAnt(Ant * ant); 
+		void killAnt(Ant * ant); 
+		Ant * extractAnt(Ant * ant); 
+		std::map<std::string, int> numAnts(); 
+		void incrementPheromone(); 
+		std::string print(); 
+}; 
