02fe6139eca974c73833c1eaaf4f1ce855bffa29
tiagodsilva <tdsh97@gmail.com>
2022-04-06 21:37:35 -0300

    Throw exception in appropriate conditions

--- include/map.cpp
+++ include/map.cpp
@@ -184,6 +184,10 @@ Ant * Map::getAntInTile(int x, int y, std::string colony) {
 	if (tileAnts->find(colony) != tileAnts->end()) { 
 		std::stack<Ant*> * colonyAnts = tileAnts->find(colony)->second; 
 		return colonyAnts->top(); 
+	} else { 
+		throw AntNotFound("There is no ant in the tile (" + 
+				std::to_string(x) + "," + std::to_string(y) + ") from the 
+				colony " + colony); 
 	} 
 	
 	return NULL; 
