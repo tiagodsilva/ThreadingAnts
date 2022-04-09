e1f2f7cb623f6bd98c8792bdbdd66869d2339e84
tiagodsilva <tdsh97@gmail.com>
2022-04-08 21:26:29 -0300

    Update list of exceptions

--- include/headers/exceptions.hpp
+++ include/headers/exceptions.hpp
@@ -21,4 +21,23 @@ class AntNotFound: public std::exception {
 			return msg.c_str(); 
 		} 
 }; 
+
+class BorderError: public std::exception { 
+	private: 
+		std::string msg; 
+	public: 
+		/**  
+		* Constructor method for `BorderError`.  
+		*/  
+		BorderError(std::string raiseMsg)
+			: msg(raiseMsg) 
+			{} 
+
+		/**  
+		* Check whether the coordinates are contemplated in the map.  
+		*/  
+		virtual const char * what() const noexcept override { 
+			return msg.c_str(); 
+		} 
+} 
 #endif 
