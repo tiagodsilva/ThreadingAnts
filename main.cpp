3f6805a6a1ed6a8120ae553f98c4b3bc9ab0b158
tiagodsilva <tdsh97@gmail.com>
2022-04-09 23:27:36 -0300

    Write method to parse the command line

--- main.cpp
+++ main.cpp
@@ -9,19 +9,37 @@
 #include "include/headers.h" 
 
 // command line; these are their standard values 
-#define NTHREADS std::string("9") 
+#define NTHREADS std::string("9") // quantity of threads  
+#define WIDTH std::string("5") // map's width 
+#define HEIGHT std::string("5") // map's height 
+#define FOV std::string("2") // ant's field of view 
+#define ITERATIONS std::string("99") // quantity of iterations in the game 
+#define PSURVIVAL std::string("12") // pheromone's lifetime 
+#define UFOOD std::string("12") // food's update rate 
+#define VFOOD std::string("32") // food's initial volume 
+#define CFOOD std::string("99") // quantity of ants that eat the food conveniently 
 
-#define HEIGHT std::string("5")  
-#define WIDTH std::string("5")  
-#define FOV std::string("2")  
 
-#define ITERATIONS std::string("99")  
+int nThreads, width, height, 
+    iterations, psurvival, fov, 
+    ufood, vfood; 
 
-#define PSURVIVAL std::string("12") 
-#define UFOOD std::string("12") 
-#define VFOOD std::string("32") 
-
-#define CFOOD std::string("99") 
+/**  
+* Parse the command line sent to the compiled file; it set the values that conform 
+* the game. 
+* @param InputParser * parser the command line parser 
+*/  
+void parse(InputParser * parser) { 
+	// Check the preamble to describe the variable's name 
+	nThreads = std::atoi(parser->parse("--nthreads", NTHREADS).c_str()); 
+	width = std::atoi(parser->parse("--width", WIDTH).c_str()); 
+	height = std::atoi(parser->parse("--height", HEIGHT).c_str()); 
+	iterations = std::atoi(parser->parse("--iters", ITERATIONS).c_str()); 
+	psurvival = std::atoi(parser->parse("--psurvival", PSURVIVAL).c_str()); 
+	fov = std::atoi(parser->parse("--ufood", UFOOD).c_str()); 
+	ufood = std::atoi(parser->parse("--fov", FOV).c_str()); 
+	vfood = std::atoi(parser->parse("--vfood", VFOOD).c_str()); 
+} 
 
 /**  
 * Initialize the objects, anthills and food, in the game.  
