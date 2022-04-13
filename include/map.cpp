d6db5391b0b4ab6a9e8d9dfa22d42efe5c118f7b
tiagodsilva <tdsh97@gmail.com>
2022-04-13 13:59:33 -0300

    Update method to kill an ant

--- include/map.cpp
+++ include/map.cpp
@@ -384,3 +384,19 @@ void Map::updateTiles(long unsigned int lTilesIndex, long unsigned int rTilesInd
 
 	} 
 } 
+
+/**  
+* Kill an ant; it requires, emphatically, a pointer to this instance.  
+* @param Ant * ant a pointer to the ant that we are going to kill 
+*/  
+void killAnt(Ant * ant) { 
+	// Identify the ant's tile 
+	Tile * tile = getTile(ant->getX(), ant->getY()); 
+	// Update attribute that contemplates all ants 
+	mapMutex.lock(); 
+	allAnts->erase(ant); 
+	mapMutex.unlock(); 
+
+	tile->killAnt(ant); 
+
+} 
