c7aa4892017568abe67e06a2cef34b487f10186a
tiagodsilva <tdsh97@gmail.com>
2022-04-15 00:58:26 -0300

    Insert script with use cases

---
+++ run.sh
@@ -0,0 +1,7 @@
+make 
+./main # default parameters 
+./main --nthreads 1 --iters 15 
+./main --nthreads 9 --width 5 --height 15 --iters 15 
+./main --nthreads 9 --width 5 --height 9 --fov 1 
+./main --nthreads 9 --width 5 --height 9 --ufood 3 
+./main --nthreads 9 --width 5 --height 9 --cfood 2 
