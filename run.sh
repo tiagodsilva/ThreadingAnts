92d0a06a08c4efb8555c19b02bf1fbc13bbc39aa
tiagodsilva <tdsh97@gmail.com>
2022-04-15 01:29:31 -0300

    Update instructions

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ make
 ./main --nthreads 9 --width 5 --height 9 --fov 1 
 ./main --nthreads 9 --width 5 --height 9 --ufood 3 
 ./main --nthreads 9 --width 5 --height 9 --cfood 2 
+./main --nthreads 9 --width 15 --height 13 --colonies 1,3,32:4,5,13 --foods 8,9,45
+./main --iters 15 --fight 0 
+./main --iters 12 --colonies 1,4,32:4,5,13 --height 9 
